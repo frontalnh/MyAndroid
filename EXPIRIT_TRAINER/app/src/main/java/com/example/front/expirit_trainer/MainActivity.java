a310dc06fd0b6963319804ac8003f6d92b19b27e
frontalnh <frontalnh@naver.com>
2017-04-13 18:41:41 +0900

    sdf

---
+++ EXPIRIT_TRAINER/app/src/main/java/com/example/front/expirit_trainer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.front.expirit_trainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
